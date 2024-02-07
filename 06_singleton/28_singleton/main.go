78f461b3af05d7851716aaa5755c5953edb8b692
Erdinc Mutlu <erdincmutlu@gmail.com>
2024-02-07 13:37:25 +0000

    Sec 6.28

---
+++ 06_singleton/28_singleton/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strconv"
+	"sync"
+)
+
+type singletonDatabase struct {
+	capitals map[string]int
+}
+
+func (db *singletonDatabase) GetPopulation(name string) int {
+	return db.capitals[name]
+}
+
+// Thread safety - one option is using sync.Once, other option is using init()
+// Laziness - Not guaranteed in init function, but guaranteed via sync.Once
+var once sync.Once
+var instance *singletonDatabase
+
+func GetSingletonDatabase() *singletonDatabase {
+	once.Do(func() {
+		caps, err := readData(".\\capitals.txt")
+		db := singletonDatabase{caps}
+		if err == nil {
+			db.capitals = caps
+		}
+		instance = &db
+	})
+
+	return instance
+}
+
+func readData(path string) (map[string]int, error) {
+	ex, err := os.Executable()
+	if err != nil {
+		panic(err)
+	}
+	exPath := filepath.Dir(ex)
+
+	file, err := os.Open(exPath + path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+
+	result := map[string]int{}
+
+	for scanner.Scan() {
+		k := scanner.Text()
+		scanner.Scan()
+		v, _ := strconv.Atoi(scanner.Text())
+		result[k] = v
+	}
+
+	return result, nil
+}
+
+func main() {
+	db := GetSingletonDatabase()
+	pop := db.GetPopulation("Seoul")
+	fmt.Printf("Population of Seoul is %d\n", pop)
+}
