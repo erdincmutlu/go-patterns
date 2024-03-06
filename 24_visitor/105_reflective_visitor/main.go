aa91c1f9a42f213aa2f4c29e354bc5e7f80fb164
Erdinc Mutlu <erdincmutlu@gmail.com>
2024-03-06 00:16:51 +0000

    Sec 24.105

---
+++ 24_visitor/105_reflective_visitor/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+// (1+2)+3
+type Expression interface {
+}
+
+type DoubleExpression struct {
+	value float64
+}
+
+type AdditionExpression struct {
+	left  Expression
+	right Expression
+}
+
+func Print(e Expression, sb *strings.Builder) {
+	de, ok := e.(*DoubleExpression)
+	if ok {
+		sb.WriteString(fmt.Sprintf("%g", de.value))
+	} else {
+		ae, ok := e.(*AdditionExpression)
+		if ok {
+			sb.WriteString("(")
+			Print(ae.left, sb)
+			sb.WriteString("+")
+			Print(ae.right, sb)
+			sb.WriteString(")")
+		}
+	}
+}
+
+func main() {
+	// 1+(2+3)
+	e := &AdditionExpression{
+		left: &DoubleExpression{1},
+		right: &AdditionExpression{
+			left:  &DoubleExpression{2},
+			right: &DoubleExpression{3},
+		},
+	}
+
+	sb := strings.Builder{}
+	Print(e, &sb)
+	fmt.Println(sb.String())
+}
